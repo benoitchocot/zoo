aaaba87f406e62a5fefbd33a14e4bbc55cf45843
Benoit Chocot <61710890+benoitchocot@users.noreply.github.com>
2023-11-21 17:32:55 +0100

    add cancancan & devise
    For authentification, dont forget to bundle install on the next computer

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_21_080149) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_21_162209) do
   create_table "animals", force: :cascade do |t|
     t.string "nom"
     t.string "espece"
@@ -39,6 +39,15 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_21_080149) do
     t.datetime "updated_at", null: false
     t.string "status"
     t.index ["article_id"], name: "index_comments_on_article_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
+    t.string "role"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   add_foreign_key "comments", "articles"
