7b4c070d95b8d804edc3536344100be0c21df871
Benoit Chocot <61710890+benoitchocot@users.noreply.github.com>
2023-11-27 09:43:29 +0100

    fixed all articles; modify land page with json animals

--- app/controllers/animaux_controller.rb
+++ app/controllers/animaux_controller.rb
@@ -1,6 +1,7 @@
 # app/controllers/animaux_controller.rb
 class AnimauxController < ApplicationController
   def index
-    @animaux = Animal.all
+    animaux_data = File.read('json/animaux.json')
+    @animaux = JSON.parse(animaux_data)
   end
 end
--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,10 +1,16 @@
 class ArticlesController < ApplicationController
-  before_action :authenticate_user!, except: [:index, :show]
+  before_action :authenticate_user!, except: [:index, :show, :all_articles]
   before_action :check_admin, only: [:create, :update]
 
   def index
     @articles = Article.order(created_at: :desc).limit(3) # Récupérer les 3 articles les plus récents
-    @animaux_random = Animal.order("RANDOM()").limit(10)
+
+    # Charger les données du fichier JSON
+    animaux_data = File.read('json/animaux.json')
+    animaux = JSON.parse(animaux_data)
+
+    # Sélection aléatoire de 10 animaux
+    @animaux_random = animaux.sample(10)
   end
 
   def all_articles
