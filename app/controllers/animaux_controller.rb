6f91c661f054c92a93f63bd94b9ca52a1ff023c3
Benoit Chocot <61710890+benoitchocot@users.noreply.github.com>
2023-11-27 11:21:08 +0100

    add enclos to especes

--- app/controllers/animaux_controller.rb
+++ app/controllers/animaux_controller.rb
@@ -4,16 +4,37 @@ class AnimauxController < ApplicationController
     @animaux = JSON.parse(animaux_data)
 
     especes_data = File.read('json/especes.json')
-    especes = JSON.parse(especes_data)
+    @especes = JSON.parse(especes_data)
+
+    enclos_data = File.read('json/enclos.json')
+    enclos = JSON.parse(enclos_data)
 
     @animaux_par_enclos = {}
 
     @animaux.each do |animal|
       espece = animal['espece']
 
-      especes.each do |espece_data|
+      @especes.each do |espece_data|
         if espece_data['_id'] == espece
           @animaux_par_enclos[espece] = espece_data['enclos']
+          break
+        end
+      end
+    end
+
+    @especes_par_enclos = {}
+
+    @especes.each do |espece|
+      enclos_id = espece['enclos']
+
+      # Trouver l'enclos correspondant dans les données d'enclos.json
+      enclos.each do |enclos_data|
+        if espece['enclos'] == enclos_data['_id']
+          @especes_par_enclos[espece['_id']] = {
+            'enclos_nom' => enclos_data['nom'],
+            'superficie' => enclos_data['superficie'],
+            'zone' => enclos_data['zone'],
+          }
           break
         end
       end
